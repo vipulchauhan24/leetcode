39604d7d57465ba1f66dd8bfc3fbdd089875f6af
Vipul Chauhan <43406423+vipulchauhan24@users.noreply.github.com>
2021-10-28 08:24:07 +0000

    2

--- arrays/containsDuplicate.js
+++ arrays/containsDuplicate.js
@@ -1,14 +1,24 @@
+//o(n^2)
+// var containsDuplicate = function(nums) {
+//     for(let i = 0; i < nums.length; i++){
+//       for(let j = i + 1; j < nums.length; j++){
+//         if(nums[i] === nums[j]){
+//           return true;
+//         }
+//       }
+//     }
+//     return false;
+// };
+//o(n)
 var containsDuplicate = function(nums) {
-  const len = nums.length;
-  var check = new Set();
-  for(let num of nums){
-    if(check.has(num)){
+  const set = new Set();
+  for(let i = 0; i < nums.length; i++){
+    if(set.has(nums[i])){
       return true;
-    } else{
-      check.add(num)
     }
+    set.add(nums[i])
   }
   return false;
 };
 
-console.log(containsDuplicate([1,2,3,1]))
+console.log(containsDuplicate([1,2,1]))
